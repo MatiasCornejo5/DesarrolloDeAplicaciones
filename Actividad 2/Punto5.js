3a506af990329ea0c2b4588faec62dfa4fa4f51d
Matias Cornejo <maticornejo99@gmail.com>
2024-03-26 22:44:22 -0300

    corregidos

---
+++ Actividad 2/Punto5.js
@@ -0,0 +1,20 @@
+function esPrimoYMayorQue10(numero) {
+    let resultado = false
+    if (esPrimo(numero) && numero > 10) {
+        resultado = true
+    }
+    return resultado
+  }
+function esPrimo(numero) {
+    let cantidadDivisores = 1 //todo num es divisible por 1
+    let resultado = false
+    for (let indice = 2; indice <= numero; indice++) {
+      if (numero % indice == 0) {
+        cantidadDivisores++
+      }
+    }
+    if (cantidadDivisores < 3) {
+      resultado = true
+    }
+    return resultado
+  }
